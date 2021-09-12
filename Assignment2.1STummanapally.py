d98b6e6b4fbec167173ffea7d1301aa6230778a6
SharathChandraTummanapally <85579587+SharathChandraTummanapally@users.noreply.github.com>
2021-09-12 09:29:44 -0500

    Assignment 2.1 added.

---
+++ Assignment2.1STummanapally.py
@@ -0,0 +1,38 @@
+# Assignment: ASSIGNMENT 2.1
+# Name: Tummanapally, Sharath Chandra
+# Date: 2021-09-10
+
+# Displaying Hello World.
+print("Hello World!")
+
+num1 = 10
+num2 = 20
+
+# Adding two numbers.
+print(num1 + num2)
+
+# Subtracting two numbers.
+print(num2 - num1)
+
+# Multiplying two numbers.
+print(num1 * num2)
+
+# Dividing two numbers.
+print(num2 / num1)
+
+# Concatenating two strings.
+str1 = 'Hi! '
+str2 = 'Welcome'
+print(str1 + str2)
+
+# Creating a list of 4 items.
+lst = [num1, num2, str1, str2]
+print(lst)
+
+# Appending an item to list.
+lst.append(' to Programming!')
+print(lst)
+
+# Creating a tuple with 4 items.
+tup = (num1, num2, str1, str2)
+print(tup)
